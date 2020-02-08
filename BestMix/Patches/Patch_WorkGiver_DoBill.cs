b21829a73cf677491bf5d8abc9061689c60be04b
madeline <zzzz465@naver.com>
2020-02-09 00:53:43 +0900

    add applying code to IL transpilaer

--- BestMix/Patches/Patch_WorkGiver_DoBill.cs
+++ BestMix/Patches/Patch_WorkGiver_DoBill.cs
@@ -17,7 +17,6 @@ namespace BestMix.Patches
         {
             try
             {
-
                 var original = AccessTools.Method(typeof(WorkGiver_DoBill), "TryFindBestBillIngredients");
                 var transpiler = AccessTools.Method(typeof(Patch_WorkGiver_DoBill), "Transpiler_TryFindBestBillIngredients");
                 HMinstance.Patch(original, null, null, new HarmonyMethod(transpiler));
@@ -38,6 +37,7 @@ namespace BestMix.Patches
             //does nameof() can make an error? IDK
             MethodInfo FetchLocalFields = AccessTools.Method(typeof(RegionProcessorSubtitution), RegionProcessorSubtitution.FetchLocalFieldsMethodName);
             MethodInfo FetchStaticFields = AccessTools.Method(typeof(RegionProcessorSubtitution), RegionProcessorSubtitution.FetchStaticFieldsMethodName);
+            MethodInfo UpdateData = AccessTools.Method(typeof(RegionProcessorSubtitution), RegionProcessorSubtitution.UpdateDataName);
             //h = hidden type
             var c__AnonStorey1 = AccessTools.FirstInner(workGiverType, type => type.Name.Contains("AnonStorey1"));
             var h_adjacentRegionsAvailable = AccessTools.Field(c__AnonStorey1, "adjacentRegionsAvailable");
@@ -117,6 +117,28 @@ namespace BestMix.Patches
                     continue; // next line is IL_01B2, stloc.1, line 1873
                 }
                 #endregion
+
+                if(inst.opcode == OpCodes.Call && inst.operand != null && inst.operand.ToString().Contains("BreadthFirstTraverse"))
+                { // entering IL_01C6 call, line 1881
+                    yield return inst;
+
+                    yield return new CodeInstruction(OpCodes.Ldsfld, RegionProcessorSubtitutionSingleton);
+                    
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Ldflda, h_bill); // index 3
+
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Ldflda, h_pawn); // index 4
+
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Ldflda, h_billGiver); // index 5
+
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Ldflda, h_chosen); // index 6
+
+                    yield return new CodeInstruction(OpCodes.Callvirt, UpdateData);
+                    continue;
+                }
 
                 yield return inst;
             }
